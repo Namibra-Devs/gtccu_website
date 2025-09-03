831013e06173063b37ee88f44de94737d0d9da82
kindo <kindojnr58@gmail.com>
2025-09-03 12:27:08 +0000

    link removed

--- gtccu/src/components/sections/OurTeamPage.jsx
+++ gtccu/src/components/sections/OurTeamPage.jsx
@@ -101,14 +101,7 @@ export default function OurTeamPage() {
           </motion.div>
         </div>
         
-        {/* Back Button */}
-        <Link 
-          to="/about"
-          className="absolute top-6 left-6 z-20 flex items-center text-white bg-white/20 backdrop-blur-sm rounded-full px-4 py-2 hover:bg-white/30 transition-colors"
-        >
-          <ArrowLeft className="w-5 h-5 mr-2" />
-          Back to About
-        </Link>
+       
       </div>
 
       {/* Team Grid Section */}
